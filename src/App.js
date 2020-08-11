ad964f6032970ae66bded251337a2444114cfb8f
marwen159 <marwenselmio21@gmail.com>
2020-08-11 11:08:40 +0200

    first push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import im2 from "./imageinsrc.jpg";
+import v1 from "./hans.mp4";
+import './style.css';
+// import image from '/image/imageinpublic.jpg';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+        <h1 className="title red">Your name here</h1>
+        <br />
+        <img src="./image/imageinpublic.jpg" />
+
+        <br />
+        <img src={im2} alt="im2" />
+      </div>
+      <video width={320} height={240} controls>
+        <source src={v1} type="video/mp4" />
+      </video>
     </div>
   );
 }
